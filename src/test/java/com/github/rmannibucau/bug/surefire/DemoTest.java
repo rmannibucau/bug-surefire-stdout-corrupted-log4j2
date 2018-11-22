dea5f170b0a3604f5096ba92d98ca21e5867ca46
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-11-22 15:06:29 +0100

    making it fail without log4j

--- src/test/java/com/github/rmannibucau/bug/surefire/DemoTest.java
+++ src/test/java/com/github/rmannibucau/bug/surefire/DemoTest.java
@@ -1,10 +1,10 @@
 package com.github.rmannibucau.bug.surefire;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class DemoTest {
+public class DemoTest {
     @Test
-    void run() {
+    public void run() {
         // no-op but the output is corrupted cause log4j2 uses FileDescriptor.out
     }
 }
